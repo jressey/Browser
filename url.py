f475fab94d313c80a304a122dfa994c884a546be
Joe Essey <jressey@gmail.com>
2025-02-28 21:05:04 -0500

    break up and remove dead code

--- url.py
+++ url.py
@@ -1,9 +1,16 @@
 import ssl
 import socket
 
+STANDARD_HEADERS = [
+    "Connection: close\r\n",
+    "User-Agent: SwagBroLite\r\n",
+]
+
 class URL:
     def __init__(self, url):
-        self.scheme, url = url.split("://", 1)
+        self.configured_socketcheme, url = url.split("://", 1)
+        # append so
+        # url = "example.com" becomes "example.com/"
         if "/" not in url:
             url += "/"
         self.host, url = url.split("/", 1)
@@ -13,82 +20,77 @@ class URL:
             self.port = int(port)
         self.path = "/" + url
 
+        # assume localhost
         if self.host == "":
             self.host = "localhost"
 
-        self.headers = []
-        self.headers.append("Host: {}\r\n".format(self.host))
-        self.headers.append("Connection: close\r\n")
-        self.headers.append("User-Agent: SwagBroLite\r\n")
-
-    def request(self):
-
-        request = "GET {} HTTP/1.1\r\n".format(self.path)
+        self.request = "GET {} HTTP/1.1\r\n".format(self.path)
         
-        for header in self.headers:
-            request += header
+        self.request += "Host: {}\r\n".format(self.host)
+        for header in STANDARD_HEADERS:
+            self.request += header
 
-        request += "\r\n"
+        self.request += "\r\n"
 
-        s = socket.socket(
+        print("Making request to:\r\n{}".format(self.request))
+
+        self.configured_socket = socket.socket(
             family=socket.AF_INET,
             type=socket.SOCK_STREAM,
             proto=socket.IPPROTO_TCP,
         )
 
-        print(request)
+    def submit_request(self):
 
-        s.connect((self.host, self.port))
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
-        
-        s.send(request.encode("utf-8"))
-
-        response = s.makefile("r", encoding="utf-8", newline="\r\n")
-
-        statusline = response.readline()
-        version, status, explanation = statusline.split(" ", 2)
+        self.configured_socket.connect((self.host, self.port))
+        self.configured_socket.send(self.request.encode("utf-8"))
+        response = self.configured_socket.makefile("r", encoding="utf-8", newline="\r\n")
 
         response_headers = {}
         while True:
             line = response.readline()
             if line == "\r\n":
                 break
-            header, value = line.split(":", 1)
-            response_headers[header.lower()] = value.strip()
+            try:
+                header, value = line.split(":", 1)
+                response_headers[header.lower()] = value.strip()
+            except ValueError:
+                print("The line: \"{}\" was ignored as a header".format(line.strip()))
+                continue
 
         assert "transfer-encoding" not in response_headers
         assert "content-encoding" not in response_headers
 
         content = response.read()
-        s.close()
+        self.configured_socket.close()
 
         return content
         
 class HttpURL(URL):
     def __init__(self, url):
-        super().__init__(url)
         self.port = 80
+        super().__init__(url)
 
-    def request(self):
-        return super().request()
+    def submit_request(self):
+        return super().submit_request()
 
 class HttpsURL(URL):
     def __init__(self, url):
-        super().__init__(url)
         self.port = 443
+        super().__init__(url)
 
-    def request(self):
-        return super().request()
+    def submit_request(self):
+        ctx = ssl.create_default_context()
+        self.configured_socket = ctx.wrap_socket(self.configured_socket, server_hostname=self.host)
+        return super().submit_request()
 
 class FileURL(URL):
     def __init__(self, url):
-        super().__init__(url)
         self.port = 8000
+        super().__init__(url)
 
-    def request(self):
-        return super().request()
+    def submit_request(self):
+        return super().submit_request()
 
 def show(body):
     in_tag = False
@@ -101,13 +103,12 @@ def show(body):
             print(c, end="")
 
 def load(url):
-    body = url.request()
+    body = url.submit_request()
     show(body)
 
 if __name__ == "__main__":
     import sys
     scheme = sys.argv[1].split("://", 1)[0]
-    print(scheme)
     if scheme == "http":
         load(HttpURL(sys.argv[1]))
     elif scheme == "https":
